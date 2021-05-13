14bcf0eefe5443ced34624b5d3a4109391c70d06
Zayd Carelse <zayd.carelse@gmail.com>
2021-05-13 10:30:27 +0200

    Initialisation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DebuggingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
